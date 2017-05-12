37cad5ef8d24494f70d5de8cc743a99f2b0974a5
Baron <mve.karlsson@gmail.com>
2017-05-12 09:50:04 +0200

    Refactor bale stacks as registered objects

--- scripts/balespawn.lua
+++ scripts/balespawn.lua
@@ -1,75 +1,99 @@
 --
---	bale spawn specialization v1.17 beta
+--	BaleSpawner object
 --
---  ONLY PRIVATE EDITS ALLOWED
---
--- 	by baron (mve.karlsson@gmail.com)
+-- 	By: baron (mve.karlsson@gmail.com)
 --
 
-BaleSpawner = {};
+BaleSpawner = {}
+BaleSpawner_mt = Class(BaleSpawner, Object)
+getfenv(0)["BaleSpawner"] = BaleSpawner
 
-function BaleSpawner.prerequisitesPresent(specializations)
-    return true;
-end;
+InitObjectClass(BaleSpawner, "BaleSpawner")
 
-function BaleSpawner:load(savegame)
-
-	self.baleParam = {};
-	self.baleParam.fillLevel   	= Utils.getNoNil(getXMLInt(self.xmlFile,"vehicle.stack#fillLevel"),4000);
-	self.baleParam.isWrapped	= Utils.getNoNil(getXMLBool(self.xmlFile,"vehicle.stack#isWrapped"),false);
-	self.baleParam.baleFilename	= getXMLString(self.xmlFile,"vehicle.stack#baleFilename");
-	
-	self.bale_indices = {};
-	
-	local count = 0;
-	while true do
-		local key = "vehicle.stack.bale("..tostring(count)..")";
-				
-		if not hasXMLProperty(self.xmlFile,key) then
-			break;
-		end;
-		
-		local index = Utils.indexToObject(self.components,getXMLString(self.xmlFile,key.."#index"));
-		table.insert(self.bale_indices,index);
-		
-		count = count + 1;
-	end;
-	
-end;
+function BaleSpawner:new(isServer, isClient, customMt)
+    local mt = customMt
+    if mt == nil then
+        mt = BaleSpawner_mt
+    end
+    
+    local self = Object:new(isServer, isClient, mt)
+    registerObjectClassName(self, "BaleSpawner")
+    
+    return self
+end
 
 function BaleSpawner:delete()
-end;
+    unregisterObjectClassName(self)
+    BaleSpawner:superClass().delete(self)
+end
 
-function BaleSpawner:mouseEvent(posX, posY, isDown, isUp, button)
-end;
+function BaleSpawner:load(_ , x, y, z, rx, ry, rz, xmlFilename)
+    local _, baseDirectory = getModNameAndBaseDirectory(xmlFilename)
+    local xmlFile = loadXMLFile("tempObjectXML", xmlFilename)
 
-function BaleSpawner:keyEvent(unicode, sym, modifier, isDown)
-end;
+    if xmlFile ~= nil then
+        -- load xml parameters
+        local stackI3DFilename 
+        local baleParam = {}
+
+        stackI3DFilename = Utils.getFilename(getXMLString(xmlFile, "object.filename"), baseDirectory)
+        baleParam.fillLevel = Utils.getNoNil(getXMLInt(xmlFile, "object.stack#fillLevel"), 4000)
+        baleParam.isWrapped = Utils.getNoNil(getXMLBool(xmlFile, "object.stack#isWrapped"), false)
+        baleParam.baleFilename = Utils.getFilename(getXMLString(xmlFile, "object.stack#baleFilename"), baseDirectory)
+
+        -- load i3d to find bale nodes
+        local stackRoot = Utils.loadSharedI3DFile(stackI3DFilename)
+        local stackNode = getChildAt(stackRoot, 0)
+        local stackTransform = createTransformGroup("stackTransform")
+
+        setTranslation(stackTransform, x, y, z)
+        setRotation(stackTransform,rx,ry,rz)
+        link(stackTransform,stackNode)
+
+        delete(stackRoot)
+
+        -- spawn this stack of bales
+        local i = 0
+        while true do
+            local key = "object.stack.bale("..tostring(i)..")"
+            if hasXMLProperty(xmlFile, key) then
+                local baleNode = Utils.indexToObject(stackNode,getXMLString(xmlFile, key.."#index"));
+                local x, y, z = getWorldTranslation(baleNode)
+                local rx, ry, rz = getWorldRotation(baleNode)
+                
+                local baleObject = Bale:new(self.isServer, self.isClient)
+                baleObject:load(baleParam.baleFilename, x, y, z, rx, ry, rz, baleParam.fillLevel)
+                baleObject:register()
+                
+                baleObject.isBuyBale = true -- flag this bale as purchased
+                
+                if baleParam.isWrapped then
+                    baleObject:setWrappingState(1)
+                end
+                
+                i = i + 1
+            else
+                break
+            end
+        end
+
+        -- cleanup
+        delete(xmlFile)
+        delete(stackTransform)
+
+        Utils.releaseSharedI3DFile(stackI3DFilename, nil, true)
+
+        return true
+    end
+
+    return false
+end
+
+function BaleSpawner:setFillLevel(fillLevel, setDirty)
+end
 
 function BaleSpawner:update(dt)
-	
-	if self.isServer and g_currentMission.vehiclesToDelete[self] == nil then --make sure this stack is not sold
-		--flag this dummy vehicle for removal
-		g_currentMission.vehiclesToDelete[self] = self;
-		
-		for k in pairs(self.bale_indices) do --spawn this stack
-			local x,y,z 	= getWorldTranslation(self.bale_indices[k]);
-			local rx,ry,rz 	= getWorldRotation	 (self.bale_indices[k]);
-			local bale 		= Bale:new(self.isServer,self.isClient);
-			
-			bale:load(Utils.getFilename(self.baleParam.baleFilename, self.baseDirectory),x,y,z,rx,ry,rz,self.baleParam.fillLevel);
-			bale:register();
-			--no way to check for errors?
-	
-			if self.baleParam.isWrapped == true then 	
-				bale:setWrappingState(1);
-			end;
-		end;
-	end;
-end;
-
-function BaleSpawner:updateTick(dt)
-end;
-
-function BaleSpawner:draw()
-end;
+    if self.isServer then
+        self:delete()
+    end
+end
